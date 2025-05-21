068e5ac4dcc9d9e67826e57fe81304de1674c3ac
lokesh awasthi <awasthi21.lokesh@gmail.com>
2025-05-21 16:26:49 +0530

    frontend commit30

--- src/Components/Pages/Booking/Booking.jsx
+++ src/Components/Pages/Booking/Booking.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import loadRazorpay from "../utils/loadRazorpay";
+import loadRazorpay from "../../../utils/loadRazorpay";
 
 const Booking = () => {
   const { tourId } = useParams();
